4afe6b7eba7aec161fba7a8e38fb7eec10c9e5a9
Ahmet Unsal <83413535+ahmetunsal@users.noreply.github.com>
2024-01-05 21:17:04 +0300

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+// create a config.js file in the root of the server folder
+//
+module.exports = {
+    mongoURI: "mongodb+srv://ahmetunsal:ahmetunsal@cluster0.sdccxv8.mongodb.net/test2?retryWrites=true&w=majority",
+};
+
