aee0e282cd1ba0bcfbfac6412522816b38a42ddd
Ahmet Unsal <83413535+ahmetunsal@users.noreply.github.com>
2024-01-05 21:17:22 +0300

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,3 @@
-// create a config.js file in the root of the server folder
 //
 module.exports = {
     mongoURI: "mongodb+srv://username:passwd@cluster0.sdccxv8.mongodb.net/test2?retryWrites=true&w=majority",
